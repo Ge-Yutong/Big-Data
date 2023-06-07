245d79cd3eeb84a26a4123b867726890b3a02e57
Ge-Yutong <85380691+Ge-Yutong@users.noreply.github.com>
2023-06-07 11:07:35 -0700

    Add files via upload

---
+++ top_n_words_Hadoop/chain_mapreduce/TopNWordsDriver.java
@@ -0,0 +1,92 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.chain.ChainReducer;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.fs.FileUtil;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TopNWordsDriver {
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        //set configuration for mapreduce
+        conf.set("mapreduce.map.memory.mb", "1024");
+        conf.set("mapreduce.reduce.memory.mb", "1024");
+        conf.set("mapreduce.task.timeout", "600000");
+        conf.set("mapreduce.task.io.sort.mb", "256");
+        conf.set("mapreduce.map.java.opts", "-Xmx1638m");
+        conf.set("mapreduce.reduce.java.opts", "-Xmx1638m");
+        conf.setInt("mapreduce.job.maps", 130);
+        conf.setInt("mapreduce.job.reduces", 1);
+        //args
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 3) {
+            System.err.println("Usage: TopNWordsDriver <stopwords> <in> <out>");
+            System.exit(2);
+        }
+        FileSystem fs = FileSystem.get(conf);
+        //delete final output path
+        Path finalOutput = new Path(args[2]);
+        if (fs.exists(finalOutput)) {
+            fs.delete(finalOutput, true);
+        }
+        //set configuration of job1(map and count)
+        Job job = Job.getInstance(conf, "top n words");
+        job.setJarByClass(TopNWordsDriver.class);
+
+        ChainMapper.addMapper(job, TopNWordsMapper.WordFrequencyMapper.class, Object.class, Text.class, Text.class, IntWritable.class, conf);
+        job.setCombinerClass(TopNWords.TopNWordsCombiner.class);
+        ChainMapper.addMapper(job, TopNWordsMapper.PassThroughMapper.class, Text.class, IntWritable.class, Text.class, IntWritable.class, conf);
+        ChainReducer.setReducer(job, TopNWords.TopNWordsReducer.class, Text.class, IntWritable.class, Text.class, IntWritable.class, conf);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.addCacheFile(new Path(otherArgs[0]).toUri());
+        FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+        //set configuration of job2(sort)
+
+
+        long startTime = System.currentTimeMillis();
+        boolean jobSuccess = job.waitForCompletion(true);
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        if (jobSuccess) {
+            long inputRecords = job.getCounters().findCounter(CounterType.INPUT_RECORDS).getValue();
+            long outputRecords = job.getCounters().findCounter(CounterType.OUTPUT_RECORDS).getValue();
+
+            // Print the counters to the console
+            System.out.println("Input records: " + inputRecords);
+            System.out.println("Output records: " + outputRecords);
+            System.out.println("Elapsed time: " + elapsedTime + " ms");
+
+            double throughput = (double) inputRecords / (elapsedTime / 1000.0);
+            System.out.println("Throughput: " + throughput + " records/sec");
+
+            // Save the counters to a file
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("counters.txt"))) {
+                writer.write("Input records: " + inputRecords + "\n");
+                writer.write("Output records: " + outputRecords + "\n");
+                writer.write("Elapsed time: " + elapsedTime + " ms\n");
+                writer.write("Throughput: " + throughput + " records/sec\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public enum CounterType {
+        INPUT_RECORDS,
+        OUTPUT_RECORDS
+    }
+}
